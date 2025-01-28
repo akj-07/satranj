13dd5b14e1d6867ba61816d2b5bc29f4b5890b41
akj-07 <ankur101245@gmail.com>
2025-01-28 17:51:28 +0530

    built: Updated packages

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}', // Add this line if you're using the 'src' folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
