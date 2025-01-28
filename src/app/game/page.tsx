47d201411689aa0f85f7aaae745ddb58922eed94
akj-07 <ankur101245@gmail.com>
2025-01-28 23:44:19 +0530

    Fix: Fixed game background

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,10 +3,11 @@ import ChessBoard from "./ChessBoard";
 // src/app/game/page.tsx
 export default function GamePage() {
     return (
-        <div className="bg-neutral-500 w-full h-full" >
-            <h2 className="text-xl font-bold">My Chess Game</h2>
+        <>
+        <div className="bg-zinc-500 w-screen h-screen" >
             <ChessBoard />
-            <footer>Shatranj</footer>
         </div>
+        </>
+        
     );
 }
