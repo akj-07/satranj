3973e2af5a576d77943e3f43a1300f756fc8e019
akj-07 <ankur101245@gmail.com>
2025-01-29 11:34:58 +0530

    feat: Updated ChessBoard by adding pieces

--- src/app/game/ChessBoard.tsx
+++ src/app/game/ChessBoard.tsx
@@ -1,23 +1,31 @@
+import ChessPiece from "./Chesspiece";
+
 export default function ChessBoard() {
+  const initialBoard: (string | null)[][] = [
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+  ];
+
   return (
-    <>
-      <div className="w-[90vh] h-[90vh] bg-zinc-500 ">
-        <div className="border-gray-800 border-4 grid grid-cols-8 w-full h-full">
-          {/* Creating a simple 8x8 grid for the chessboard */}
-          {Array.from({ length: 8 }).map((_, rowIndex) =>
-            Array.from({ length: 8 }).map((_, colIndex) => (
-              <div
-                key={`${rowIndex}-${colIndex}`}
-                className={`w-[100%] h-[100%] ${
-                  (rowIndex + colIndex) % 2 === 0
-                    ? "bg-gray-300"
-                    : "bg-gray-600"
-                }`}
-              ></div>
-            ))
-          )}
-        </div>
-      </div>
-    </>
+    <div className="w-[90vw] max-w-[600px] mx-auto grid grid-cols-8 border-4 border-gray-800">
+      {initialBoard.map((row, rowIndex) =>
+        row.map((piece, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className={`relative flex items-center justify-center w-full aspect-square ${
+              (rowIndex + colIndex) % 2 === 0 ? "bg-gray-300" : "bg-gray-600"
+            }`}
+          >
+            <ChessPiece piece={piece} />
+          </div>
+        ))
+      )}
+    </div>
   );
 }
