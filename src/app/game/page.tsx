63839e691f0c2117ee851dcd284acc0c51fa8408
akj-07 <ankur101245@gmail.com>
2025-01-29 18:06:49 +0530

    feat: Updated game page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,13 +1,10 @@
 import ChessBoard from "./ChessBoard";
 
-// src/app/game/page.tsx
 export default function GamePage() {
     return (
-        <>
-        <div className="bg-zinc-500 w-screen h-screen" >
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+            <h1 className="text-2xl font-bold mb-4">My Chess Game</h1>
             <ChessBoard />
         </div>
-        </>
-        
     );
 }
